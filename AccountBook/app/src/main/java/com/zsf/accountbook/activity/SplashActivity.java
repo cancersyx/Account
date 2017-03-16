c2014db8a9d5c6d16fd6144dbbb48a19f2696af9
zsf <852333743@qq.com>
2017-03-16 23:39:09 +0800

    修改了SplashActivity为全屏显示

--- AccountBook/app/src/main/java/com/zsf/accountbook/activity/SplashActivity.java
+++ AccountBook/app/src/main/java/com/zsf/accountbook/activity/SplashActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 import com.zsf.accountbook.constans.AppConstants;
 import com.zsf.accountbook.R;
@@ -17,6 +18,8 @@ public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
 
         // 判断是否是第一次开启应用
